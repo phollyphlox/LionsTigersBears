8c6c0e3ef6c45c2b7832e44222bc62137dd190a3
Apple28 <tstc-user@tstc-users-iMac.local>
2017-02-01 12:38:28 -0600

    second revision

---
+++ Bear.swift
@@ -0,0 +1,9 @@
+//
+//  Bear.swift
+//  LionsTigersBears
+//
+//  Created by Apple28 on 1/31/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+import Foundation
---
+++ Tiger.swift
@@ -0,0 +1,9 @@
+//
+//  Tiger.swift
+//  LionsTigersBears
+//
+//  Created by Apple28 on 1/31/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+import Foundation
